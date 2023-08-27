ad3a789890cc94f7bcd129dcf399b8bef5a8676e
liorshalom86 <liorshalom86@gmail.com>
2023-08-27 19:13:15 +0300

    downloader fix

--- src/downloader.cpp
+++ src/downloader.cpp
@@ -4,23 +4,30 @@
 
 namespace m_player{
 
-Downloader::Downloader(std::string a_song, std::string a_path)
-: m_path(a_path)
-, m_file_name(a_song)
+Downloader::Downloader()
+: m_link("")
+{
+    
+}
+
+bool Downloader::run(std::string a_song, std::string a_path)
 {
     std::string result = find_url(a_song);
     if(result != "Fail"){
         generate_link(result);
     }
+    download(a_song, a_path);
     std::cout <<m_link<< '\n';
+    
+    return true;
 }
 
-
-bool Downloader::download()
+bool Downloader::download(std::string a_song, std::string a_path)
 {
-    std::string output = m_path + "/" + m_file_name + ".%(ext)s";
+    std::string output = a_path + "/" + a_song + ".%(ext)s";
     std::string command = "yt-dlp -x --audio-format mp3 -o \"" + output + "\" \"" + m_link + "\"";
-    std::string dir_cmnd = "mkdir \"" + m_path + "\"";
+    
+    std::string dir_cmnd = "mkdir \"" + a_path + "\"";
     std::system(dir_cmnd.c_str());
 
     FILE* pipe = popen(command.c_str(), "r");
@@ -38,7 +45,6 @@ bool Downloader::download()
         {
             return false;
         }
-        // std::cout << buffer;
     }
 
     int status = pclose(pipe);
@@ -71,14 +77,12 @@ std::string find_url(const std::string& a_song)
     return result;
 }
 
-
-
 bool Downloader::generate_link(std::string& a_string)
 {
 
     if (!a_string.empty() && a_string != "No search result URLs found."){
         a_string = trim(a_string);
-        // std::cout << "URL of the first search result:555 " << a_string << std::endl;
+        
         m_link = a_string;
     } else{
         std::cout << "No search result URLs found2." << std::endl;
@@ -86,11 +90,6 @@ bool Downloader::generate_link(std::string& a_string)
 
     return true;
 }
-
-// std::string Downloader::get_link()
-// {
-//     return m_link;
-// }
 
 std::string trim(const std::string& str) {
     size_t first = str.find_first_not_of(" \t\n");
@@ -104,25 +103,4 @@ std::string trim(const std::string& str) {
 
 
 
-/*
-#include <iostream>
-#include <cstdlib>
-#include <thread>
-#include <sys/wait.h>
 
-
-int main() {
-    const char* youtubeUrl = "https://www.youtube.com/watch?v=-w-2K9U8UIs&pp=ygUKZ29sZGVuIGJveQ%3D%3D";
-    const char* command = "yt-dlp -x --audio-format mp3 -o \"goldy888.mp3\"";
-
-    // Combine the command and YouTube URL
-    std::string fullCommand = std::string(command) + " " + youtubeUrl;
-
-    // Execute the command
-    std::system(fullCommand.c_str());
-
-
-
-
-    return 0;
-}*/
